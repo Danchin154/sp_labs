8cc85279458b0d819572640d95caba5bcd2603bf
Danchin154 <73567310+Danchin154@users.noreply.github.com>
2023-04-22 14:29:44 +0800

    Add files via upload

---
+++ lab2/2/Server/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+            // Create a TCP socket.
+            Socket client = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+            // Connect the socket to the remote endpoint.
+            try
+            {
+                client.Connect(ipEndPoint);
+                if (client.Connected == true)
+                    Console.WriteLine("Connected");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            // There is a text file test.txt located in the root directory.
+            string fileName = "C:\\test\\example.txt";
+
+            // Send file fileName to remote device
+            Console.WriteLine("Sending {0} to the host.", fileName);
+            client.SendFile(fileName);
+
+            // Release the socket.
+            client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+    }
+}
